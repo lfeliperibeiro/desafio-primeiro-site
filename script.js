e1bcd874a27664f67bf0d027865022bb3b39cbf9
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-27 21:54:36 -0300

    update
    criação de modal e troca dinamica do conteudo com JS

---
+++ script.js
@@ -0,0 +1,21 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
+const modal = document.querySelector(".modal");
+
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const pageId = card.getAttribute("id");
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector(
+      "iframe"
+    ).src = `https://rocketseat.com.br/${pageId}`;
+  });
+}
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+});
+
+document.querySelector('.maximize-modal').addEventListener('click', () =>{
+    modal.classList.add('max')
+})
