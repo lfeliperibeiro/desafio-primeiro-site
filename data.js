db0d8dead97c042326a23a44354a55f16e45c5c3
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-29 17:52:47 -0300

    Update
    Textos dinamicos vindo do backend

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: "starter",
+    num: 01,
+    description:
+      '<span class="color-starter">Torne-se um programador desejado</span> com esses cursos gratuitos',
+    role: "10 módulos",
+    price: "Gratuito",
+  },
+  {
+    id: "launchbase",
+    num: 02,
+    description:
+      '<span class="color-launch">Domine programação do zero</span> e tenha acesso às melhores opertunindades do mercado',
+    role: "10 módulos",
+    price: "Pago",
+  },
+  {
+    id: "gostack",
+    num: 03,
+    description:
+      '<span class="color-go">Treinamento imersivo</span> nas tecnologias mais modernas de desenvolvimento web e mobile',
+    role: "10 módulos",
+    price: "Pago",
+  },
+];
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 
 const server = express();
+const data = require("./data");
 nunjucks.configure("views", {
   express: server,
+  autoescape: false,
 });
 
 server.set("view engine", "njk");
@@ -11,16 +13,30 @@ server.set("view engine", "njk");
 server.use(express.static("public"));
 
 server.get("/", (req, res) => {
-  return res.render("about");
+  about = {
+    image: "https://rocketseat.com.br/static/images/logo-rocketseat.svg",
+    title: "Rocketseat",
+    imagetech: "https://rocketseat.com.br/static/images/update/trofeu-home.svg",
+    description:
+      "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+    uls: [
+      { name: "JavaScript" },
+      { name: "Node.js" },
+      { name: "React" },
+      { name: "React-Native" },
+    ],
+  };
+
+  return res.render("about", { about });
 });
 
 server.get("/courses", (req, res) => {
-  return res.render("courses");
+  return res.render("courses", { items: data });
 });
 
-server.use((req, res) =>{
-    res.status(404).render('not-found')
-})
+server.use((req, res) => {
+  res.status(404).render("not-found");
+});
 
 server.listen(3000, () => {
   console.log("server running");
